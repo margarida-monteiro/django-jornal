da10643a2cb3ca21b160b0e65ed5ce6b485cde24
Margarida <margaridamonteiro2709@gmail.com>
2026-04-12 23:03:22 +0100

    adicionar funcionalidades de artigos e comentários, incluindo views, templates e URLs

--- noticias/views.py
+++ noticias/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Artigo, Comentario
 
-# Create your views here.
+def lista_artigos(request):
+    artigos = Artigo.objects.all()
+    return render(request, 'noticias/lista_artigos.html', {'artigos': artigos})
+
+
+def detalhe_artigo(request, artigo_id):
+    artigo = get_object_or_404(Artigo, pk=artigo_id)
+    return render(request, 'noticias/detalhe_artigo.html', {'artigo': artigo})
+
+
+def comentarios_artigo(request, artigo_id):
+    artigo = get_object_or_404(Artigo, pk=artigo_id)
+    comentarios = artigo.comentarios.all()
+
+    if request.method == 'POST':
+        texto = request.POST.get('texto')
+        if texto:
+            Comentario.objects.create(artigo=artigo, texto=texto)
+            return redirect('comentarios_artigo', artigo_id=artigo.id)
+
+    return render(request, 'noticias/comentarios.html', {
+        'artigo': artigo,
+        'comentarios': comentarios
+    })
