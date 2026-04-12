f9d53c2739cd9fd0f8abf7fce032dad5f45754dd
Margarida <margaridamonteiro2709@gmail.com>
2026-04-12 22:51:35 +0100

    criar modelos Artigo e Comentario + registo no admin

--- noticias/models.py
+++ noticias/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Artigo(models.Model):
+    titulo = models.CharField(max_length=200)
+    corpo = models.TextField()
+    data_publicacao = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.titulo
+
+    class Meta:
+        ordering = ['-data_publicacao']
+
+
+class Comentario(models.Model):
+    artigo = models.ForeignKey(Artigo, on_delete=models.CASCADE, related_name='comentarios')
+    texto = models.TextField()
+    data_comentario = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"Comentário em {self.artigo.titulo}"
